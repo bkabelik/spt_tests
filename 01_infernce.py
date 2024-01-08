cb7c9d6cbd92492fe869efcb318008c3cd2b2f78
daktar <bk@kabelik.at>
2024-01-08 12:48:53 +0100

    first commit

---
+++ 01_infernce.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# Add the project's files to the python path
+file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # for .py script
+#file_path = os.path.dirname(os.path.abspath(''))  # for .ipynb notebook
+sys.path.append(file_path)
+sys.path.append("/home/daktar/superpoint_transformer")
+
+# Necessary for advanced config parsing with hydra and omegaconf
+from omegaconf import OmegaConf
+OmegaConf.register_new_resolver("eval", eval)
+
+import hydra
+from src.utils import init_config
+import torch
+from src.visualization import show
+from src.datasets.dales import CLASS_NAMES, CLASS_COLORS
+from src.datasets.dales import DALES_NUM_CLASSES as NUM_CLASSES
+from src.transforms import *
+
+# Parse the configs using hydra
+cfg = init_config(overrides=[
+    "experiment=dales",
+    "ckpt_path=/home/daktar/superpoint_transformer/logs/train/runs/2024-01-05_11-00-33/checkpoints/epoch_279.ckpt"
+])
